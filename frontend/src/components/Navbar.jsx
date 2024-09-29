9876e14fd9b508052b5f6a66c9de8aa428152b43
Parth Patil <psp90804@gmail.com>
2024-09-29 08:15:55 +0530

    changed logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     const navigate = useNavigate();
     return (
         <nav className="h-20 flex justify-items-start items-center bg-slate-50 mb-5 sticky top-0 w-full z-10" style={{boxShadow: "0 0 20px rgba(0,0,0,0.2)"}}>
-            <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="Google Logo" height="50" width="80" className="mx-5" onClick={() => navigate("/")} />
+            <img src="/logo512.png" alt="Google Logo" height="70" width="70" className="mx-5" onClick={() => navigate("/")} />
         </nav>
     )
 }
